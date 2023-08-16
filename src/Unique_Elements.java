f1475e5b1245298967a925cc147bd42bd72f2126
Saksham <b0296269@airtel.com>
2023-08-16 13:09:13 +0530

    First commit for unique Elements

--- src/Unique_Elements.java
+++ src/Unique_Elements.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Unique_Elements {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Unique_Elements {
+    public static void main(String[] args) {
+        ArrayList<Integer> Ele= new ArrayList<Integer>();
+        Scanner cin = new Scanner(System.in);
+        System.out.println("Input Digits");
+        int n= cin.nextInt();
+        for (int i=0;i<n;i++){
+            Ele.add(cin.nextInt());
+        }
+        Set s= new HashSet(Ele);
+        System.out.println("All elements : " + Ele);
+        System.out.println("Unique Elements : " + s);
+    }
 }
